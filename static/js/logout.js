4fbd1f1e031b3e4654e38c0e4b3bebb85c928eec
enzosamojedny <enzosamojedny@gmail.com>
2024-02-05 15:17:16 -0300

    fix: frontend now correctly handles logout

--- static/js/logout.js
+++ static/js/logout.js
@@ -2,17 +2,9 @@ const logoutBtn = document.getElementById("logout-btn");
 
 async function logout() {
   try {
-    const response = await axios.delete("/api/logout");
-    if (response.status >= 200 && response.status < 300) {
-      setTimeout(() => {
-        window.location.href = "/login"; //not redirecting
-      }, 3000);
-    } else {
-      console.error("Logout failed");
-    }
+    await axios.delete("/api/logout");
   } catch (error) {
-    console.error("Error during logout:", error);
-    throw new Error("Logout failed", error);
+    window.location.href = "/login";
   }
 }
 
