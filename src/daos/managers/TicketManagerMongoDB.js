3395f368e8f2e8e96dd13182299e0686b113fc37
enzosamojedny <enzosamojedny@gmail.com>
2024-02-06 17:34:03 -0300

    fix: added more error handling, removed unique fields in ticket

--- src/daos/managers/TicketManagerMongoDB.js
+++ src/daos/managers/TicketManagerMongoDB.js
@@ -1,32 +1,32 @@
 const Ticket = require("../models/Ticket");
-const Cart = require("../models/Cart");
 
 class TicketManagerMongoDB {
   async createTicket(cartId, userData) {
     try {
-      //! con el cartId tengo que buscar en DB, comprobar si hay suficientes productos
-      //! restarlos, ( si hay, sino, devolver los productos, tmb si son invalidos)
-
-      if (cartId) {
-        const purchaser = userData.email;
-        const amount = userData.products.reduce((total, product) => {
-          return total + product.quantity * product.price;
-        }, 0);
-        
-        try {
-          const ticketCreated = await Ticket.create({
-            purchaser: purchaser,
-            amount: amount,
-          });
-          return { payload: ticketCreated.toObject(), status: "created" };
-        } catch (error) {
-          throw new Error("Unable to create ticket");
-        }
+      if (!cartId || !userData) {
+        throw new Error("Invalid cartId or userData");
       }
+
+      const purchaser = userData.email;
+      const amount = userData.products.reduce((total, product) => {
+        return total + product.quantity * product.price;
+      }, 0);
+
+      const ticket = new Ticket({
+        purchaser: purchaser,
+        amount: amount,
+      });
+
+      await ticket.validate();
+
+      const ticketCreated = await ticket.save();
+
+      return { payload: ticketCreated.toObject(), status: "created" };
     } catch (error) {
-      throw new Error("Invalid cartId or userData");
+      throw new Error("Error creating ticket: " + error.message);
     }
   }
+
   async getTicketByEmail(userEmail) {
     const found = await Ticket.findOne({ email: userEmail }).lean();
     if (!found) {
--- src/daos/models/Ticket.js
+++ src/daos/models/Ticket.js
@@ -4,13 +4,12 @@ const TicketSchema = new mongoose.Schema({
   _id: { type: String, default: () => new mongoose.Types.ObjectId() },
   code: {
     type: String,
-    required: true,
     unique: true,
     default: () => randomUUID(),
   },
   purchase_datetime: { type: Date, default: Date.now, index: true },
   amount: { type: Number, required: true },
-  purchaser: { type: String, required: true, unique: true },
+  purchaser: { type: String, required: true },
 });
 
 TicketSchema.index([{ purchase_datetime: 1, amount: 1 }]);
